a3dfe92e7f8cbc30929f9e7fd7370bcb845cfa6d
CarlosManoelWendorff <carlos.wendorff@catolicasc.edu.br>
2023-10-19 09:55:58 -0300

    uncommited changes

--- box_ui/pac_4/lib/main.dart
+++ box_ui/pac_4/lib/main.dart
@@ -24,6 +24,51 @@ class MyApp extends StatelessWidget {
         '/register': (context) => const RegistrationPage(),
         '/card-register': (context) => const RegisterScreen(),
       },
+      theme: ThemeData(
+        primaryColor: Colors.blue[800],
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(
+          color: Colors.blue[800], // Set the default icon color to green
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue[800]
+        ),
+        appBarTheme: AppBarTheme(backgroundColor:  Colors.blue[800]),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue[800]), // Set the default background color
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0), // Adjust the value to make it more or less circular
+              ),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme( 
+          labelStyle: TextStyle(color: Color(0xFF1565C0)),
+           // Set label text color
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color:Color(0xFF1565C0)),
+            borderRadius: BorderRadius.circular(10.0), // Border radius // Set border color when the field is enabled
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF1565C0)), // Border color when the field is focused
+            borderRadius: BorderRadius.circular(10.0), // Border radius
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey), // Default border color
+            borderRadius: BorderRadius.circular(10.0), // Border radius
+            gapPadding: 10.0, // Adjust this value to control the box shadow spread
+          ),
+          // Add a box shadow to the focused field
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red), // Border color when the field is focused
+            borderRadius: BorderRadius.circular(10.0), // Border radius
+            gapPadding: 10.0, // Adjust this value to control the box shadow spread
+          ),
+        )
+      ),
+      
     );
   }
 }
@@ -42,29 +87,50 @@ class DialogExample extends StatelessWidget {
         title: const Text('Shuffle'),
       ),
       body:
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      Stack(
         children: <Widget>[
-       TextButton(
-        onPressed:() => {
-           Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CustomCard(
-                        title: 'carta teste',
-                        description: 'Carta de teste pros guri',
-                        imagePath: './assets/lanso.jpg',
+       Hero(
+            tag: 'cardHero',
+            child: Material(
+              color: Colors.transparent,
+              child: Ink.image(
+                image: AssetImage('./assets/carta.jpg'), // Your background image
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity, // Adjust the height as needed
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CustomCard(
+                          title: 'carta teste',
+                          description: 'Carta de teste pros guri',
+                          imagePath: './assets/lanso.jpg',
+                        ),
                       ),
-                    ),
-           )
-        },
-        child: const Text('Abrir Carta'),
-      ),
-      TextButton(
-        onPressed: () => goToRegisterCard(context),
-        child: const Text('Escrever carta'),
-      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+      const Spacer(), // Add a spacer to push the button to the bottom
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20.0),
+              child: RawMaterialButton(
+                 onPressed: () => goToRegisterCard(context),
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(16.0),
+                constraints: const BoxConstraints(minWidth: 0, minHeight: 0),
+                child: Icon(Icons.add, size: 40, color: Colors.white), // Adjust size as needed
+                fillColor: Colors.blue[800],
+            ),
+            ),
+          ),
       ]
     )
     );
